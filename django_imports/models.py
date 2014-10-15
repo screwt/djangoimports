68bfc66f14fb147a3f47103f419825655b74c3a8
Benoit JUIN <benoit.juin@gmail.com>
2014-10-15 15:45:25 +0200

    test

--- django_imports/models.py
+++ django_imports/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib import admin
 
+
 class ImportModel(models.Model):
     name = models.CharField(max_length=30)
     model_name = models.CharField(max_length=30)
